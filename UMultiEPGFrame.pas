ab25eec8d70fbddfbd36a691ebcf8636d20d1a57
darktab <christian.jacoby@a3.epfl.ch>
2014-06-27 22:16:49 +0200

    more tests

--- UMultiEPGFrame.pas
+++ UMultiEPGFrame.pas
@@ -6,7 +6,7 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes,
   System.Variants,
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
-  FMXTee.Chart, FMXTee.Series.Gantt;
+  FMXTee.Chart, FMXTee.Series.Gantt, FMXTee.Procs;
 
 type
   TMultiEPGFrame = class(TFrame)
@@ -49,10 +49,42 @@ begin
 
   fGanttSeries1 := TGanttSeries.Create(self);
   fGanttSeries2 := TGanttSeries.Create(self);
-  fGanttSeries1.AddGanttColor(0, 20, 0, 'test', TAlphaColorRec.Honeydew);
-  fGanttSeries1.AddGanttColor(20, 30, 0, 'blim', TAlphaColorRec.Honeydew);
-  fGanttSeries2.AddGanttColor(0, 10, 1, 'blam', TAlphaColorRec.Salmon);
-  fGanttSeries2.AddGanttColor(10, 30, 1, 'blom', TAlphaColorRec.Darksalmon);
+  fGanttSeries1.AddGanttColor(0, 0, 0, 'TF1 HD', TAlphaColorRec.Honeydew);
+  fGanttSeries1.AddGanttColor(0, 0, 1, 'RTL HD', TAlphaColorRec.Honeydew);
+  fGanttSeries1.AddGanttColor(0, 0, 2, 'VOX', TAlphaColorRec.Honeydew);
+  fGanttSeries1.AddGanttColor(0, 0, 3, 'M6 HD', TAlphaColorRec.Honeydew);
+  fGanttSeries1.AddGanttColor(0, 0, 4, 'W9', TAlphaColorRec.Honeydew);
+  fGanttSeries1.AddGanttColor(0, 0, 5, 'France 2', TAlphaColorRec.Honeydew);
+  fGanttSeries1.AddGanttColor(0, 0, 6, 'Plug TV', TAlphaColorRec.Honeydew);
+
+  fGanttSeries2.AddGanttColor(0, 20, 0, 'test', TAlphaColorRec.Beige);
+  fGanttSeries2.AddGanttColor(20, 30, 0, 'blim', TAlphaColorRec.Honeydew);
+  fGanttSeries2.AddGanttColor(30, 60, 0, 'blim', TAlphaColorRec.Honeydew);
+
+  fGanttSeries2.AddGanttColor(0, 10, 1, 'blam', TAlphaColorRec.Beige);
+  fGanttSeries2.AddGanttColor(10, 30, 1, 'blom', TAlphaColorRec.Honeydew);
+  fGanttSeries2.AddGanttColor(30, 70, 1, 'blom', TAlphaColorRec.Honeydew);
+
+  fGanttSeries2.AddGanttColor(0, 20, 2, 'test', TAlphaColorRec.Beige);
+  fGanttSeries2.AddGanttColor(20, 30, 2, 'blim', TAlphaColorRec.Honeydew);
+  fGanttSeries2.AddGanttColor(30, 60, 2, 'blim', TAlphaColorRec.Honeydew);
+
+  fGanttSeries2.AddGanttColor(0, 20, 3, 'test', TAlphaColorRec.Beige);
+  fGanttSeries2.AddGanttColor(20, 30, 3, 'blim', TAlphaColorRec.Honeydew);
+  fGanttSeries2.AddGanttColor(30, 60, 3, 'blim', TAlphaColorRec.Honeydew);
+
+  fGanttSeries2.AddGanttColor(0, 10, 4, 'blam', TAlphaColorRec.Beige);
+  fGanttSeries2.AddGanttColor(10, 30, 4, 'blom', TAlphaColorRec.Honeydew);
+  fGanttSeries2.AddGanttColor(30, 70, 4, 'blom', TAlphaColorRec.Honeydew);
+
+  fGanttSeries2.AddGanttColor(0, 20, 5, 'test', TAlphaColorRec.Beige);
+  fGanttSeries2.AddGanttColor(20, 30, 5, 'blim', TAlphaColorRec.Honeydew);
+  fGanttSeries2.AddGanttColor(30, 60, 5, 'blim', TAlphaColorRec.Honeydew);
+
+  fGanttSeries2.AddGanttColor(0, 20, 6, 'test', TAlphaColorRec.Beige);
+  fGanttSeries2.AddGanttColor(20, 30, 6, 'blim', TAlphaColorRec.Honeydew);
+  fGanttSeries2.AddGanttColor(30, 60, 6, 'blim', TAlphaColorRec.Honeydew);
+
   fGanttSeries1.Pointer.VertSize := 20;
   fGanttSeries2.Pointer.VertSize := 20;
   fChart.AddSeries(fGanttSeries1);
@@ -63,16 +95,34 @@ begin
 
   fChart.Color := TAlphaColorRec.White;
 
+  fChart.LeftAxis.Inverted := True;
+  fChart.LeftAxis.TickOnLabelsOnly := True;
+
+  fChart.LeftAxis.Automatic := false;
+  fChart.LeftAxis.Minimum := -0.5;
+  fChart.LeftAxis.Maximum := 5;
+
+  fChart.BottomAxis.Automatic := false;
+  fChart.BottomAxis.Minimum := 0;
+  fChart.BottomAxis.Maximum := 50;
+
+  fChart.AllowZoom := false;
+  fChart.Panning.Active := True;
+  fChart.AllowPanning := TPanningMode.pmBoth;
+
+  // fChart.BottomAxis.Minimum := 0;
+  // fChart.BottomAxis.Maximum := 10;
+  // fChart.LeftAxis.Minimum := 0;
+  // fChart.LeftAxis.Maximum := 2;
+
   // fGanttSeries1.Transparency := 50;
   // fGanttSeries2.Transparency := 50;
 
-  fGanttSeries1.Marks.Visible := true;
-  fGanttSeries1.Marks.Transparent := true;
-  fGanttSeries2.Marks.Visible := true;
-  fGanttSeries2.Marks.Transparent := true;
+  // fGanttSeries1.Marks.Visible := True;
+  // fGanttSeries1.Marks.Transparent := True;
+  fGanttSeries2.Marks.Visible := True;
+  fGanttSeries2.Marks.Transparent := True;
 
-  fGanttSeries1.XLabel[0] := 'TF1 HD';
-  fGanttSeries2.XLabel[0] := 'RTL HD';
 end;
 
 end.
