a324980770e1bcfe63c52a59d0b094d8dddb624f
darktab <christian.jacoby@a3.epfl.ch>
2014-07-08 22:00:58 +0200

    load only 7 channels at once
    TChart gets slow otherwise

--- UMultiEPGFrame.pas
+++ UMultiEPGFrame.pas
@@ -31,6 +31,10 @@ type
     fGanttSeriesGuidesMax: TDateTime;
     fGanttSeriesGuidesMin: TDateTime;
 
+    fChMin: Integer;
+    fChMax: Integer;
+    procedure loadNextChannels(vChMin: Integer; vChMax: Integer);
+
   public
     { Public declarations }
     procedure init;
@@ -48,128 +52,35 @@ begin
 
 end;
 
-procedure TMultiEPGFrame.ChartScroll(Sender: TObject);
-begin
-  { if (fChart.BottomAxis.Minimum < fGanttSeriesGuidesMin) then
-    begin
-    fChart.BottomAxis.Minimum := fGanttSeriesGuidesMin;
-    fChart.BottomAxis.Maximum := fChart.BottomAxis.Minimum +
-    fGanttSeriesGuidesMax;
-    end;
-    if (fChart.BottomAxis.Maximum > 70) then
-    begin
-    fChart.BottomAxis.Minimum := 20;
-    fChart.BottomAxis.Maximum := 70;
-    end; }
-
-  if (fChart.LeftAxis.Minimum < fGanttSeriesChannelsMin) then
-  begin
-    fChart.LeftAxis.Minimum := fGanttSeriesChannelsMin;
-    fChart.LeftAxis.Maximum := fGanttSeriesChannelsMax;
-  end;
-
-  if (fChart.LeftAxis.Maximum > fChannelNumber - 0.5) then
-  begin
-    fChart.LeftAxis.Maximum := fChannelNumber - 0.5;
-    fChart.LeftAxis.Minimum := fChart.LeftAxis.Maximum - 6.5;
-  end;
-
-end;
-
-procedure TMultiEPGFrame.init;
+procedure TMultiEPGFrame.loadNextChannels(vChMin: Integer; vChMax: Integer);
 var
   lChNumber: Integer;
-  lShowCount: Integer;
-  lDefaultServiceReference: String;
-  lShowColor: Integer;
   lMarkLength: Integer;
-  lShowTitle: String;
+  lDefaultServiceReference: string;
+  lShowCount: Integer;
+  lShowColor: Integer;
+  lShowTitle: string;
 begin
-  fChart := TChart.Create(self);
-  fChart.Parent := self;
-  fChart.Align := TAlignLayout.Client;
-
-  fChart.View3D := False;
-  fChart.BottomAxis.Visible := True;
-  fChart.BottomAxis.OtherSide := True;
-
-  fChart.Frame.Visible := False;
-  fChart.Width := self.Width;
-  fChart.Height := self.Height;
-  fChart.MarginRight := 0;
-  fChart.MarginBottom := 0;
-  fChart.MarginLeft := 0;
-  fChart.MarginTop := 1;
-  fChart.LeftAxis.LabelsSize := 80;
-
+  lChNumber := vChMin;
   lMarkLength := 10;
-
-  fChart.Legend.Visible := False;
-  fChart.Border.Visible := False;
-
-  fChart.Color := TAlphaColorRec.White;
-
-  fChart.LeftAxis.Inverted := True;
-  fChart.LeftAxis.TickOnLabelsOnly := True;
-
-  fChart.LeftAxis.MinorTicks.Visible := False;
-  fChart.BottomAxis.MinorTicks.Visible := False;
-
-  fChart.AllowZoom := False;
-  fChart.AllowPanning := TPanningMode.pmBoth;
-  fChart.ScrollMouseButton := TMouseButton.mbLeft;
-  fChart.OnScroll := self.ChartScroll;
-
-  fChannelNumber := MainDataModule.DreamFDMemTableChannelList.RecordCount;
-
-  fGanttSeriesChannelsMin := -0.5;
-  fGanttSeriesChannelsMax := 5;
-
-  fChart.LeftAxis.Automatic := False;
-  fChart.LeftAxis.Minimum := fGanttSeriesChannelsMin;
-  fChart.LeftAxis.Maximum := fGanttSeriesChannelsMax;
-
-  fGanttSeriesGuidesMin := now;
-  fGanttSeriesGuidesMax := IncMinute(fGanttSeriesGuidesMin, 120);
-
-  fChart.BottomAxis.Automatic := False;
-  fChart.BottomAxis.Maximum := fGanttSeriesGuidesMax;
-  fChart.BottomAxis.Minimum := fGanttSeriesGuidesMin;
-
-  fGanttSeriesChannels := TGanttSeries.Create(self);
-  fGanttSeriesGuides := TGanttSeries.Create(self);
-  fLineSeriesNow := TLineSeries.Create(self);
-
-  if not MainDataModule.DreamFDMemTableChannelList.Active then
-  begin
-    MainDataModule.DreamFDMemTableChannelList.Open;
-    MainDataModule.DreamFDMemTableChannelList.First;
-  end;
-
-  lChNumber := 0;
+  MainDataModule.DreamFDMemTableChannelList.RecNo := vChMin;
   while not MainDataModule.DreamFDMemTableChannelList.EOF do
   begin
     fGanttSeriesChannels.AddGanttColor(now, now, lChNumber,
       AnsiLeftStr(MainDataModule.DreamFDMemTableChannelList.FieldByName
       ('servicename').AsString, 8), TAlphaColorRec.Honeydew);
-
     lDefaultServiceReference := MainDataModule.DreamFDMemTableChannelList.
       FieldByName('servicereference').AsString;
-
     if MainDataModule.DreamFDMemTableTextEPG.Active then
     begin
       MainDataModule.DreamRESTResponseDataSetAdapterTextEPG.ClearDataSet;
-
       MainDataModule.DreamRESTResponseDataSetAdapterTextEPG.Active := False;
       MainDataModule.DreamFDMemTableTextEPG.Close;
     end;
-
     MainDataModule.DreamRESTResponseDataSetAdapterTextEPG.FieldDefs.Clear;
-
     // sRef parameter
     MainDataModule.DreamRESTRequestTextEPG.Params[0].Value :=
       lDefaultServiceReference;
-
     try
       MainDataModule.DreamRESTRequestTextEPG.Execute;
     except
@@ -201,17 +112,15 @@ begin
           fGanttSeriesGuidesMax := IncMinute(fGanttSeriesGuidesMin, 120);
           fChart.BottomAxis.Maximum := fGanttSeriesGuidesMax;
         end;
-
         lMarkLength := Trunc(MainDataModule.DreamFDMemTableTextEPG.FieldByName
           ('duration_sec').AsFloat / 400) - 1;
         if lMarkLength < 0 then
         begin
           lMarkLength := 0;
         end;
-
         if lMarkLength = 0 then
         begin
-          lShowTitle := ' '
+          lShowTitle := ' ';
         end
         else
         begin
@@ -226,9 +135,7 @@ begin
             if lShowTitle = ' ...' then
               lShowTitle := ' ';
           end;
-
         end;
-
         fGanttSeriesGuides.AddGanttColor
           (UnixToDateTime(MainDataModule.DreamFDMemTableTextEPG.FieldByName
           ('begin_timestamp').AsInteger, False),
@@ -236,30 +143,124 @@ begin
           ('begin_timestamp').AsInteger + MainDataModule.DreamFDMemTableTextEPG.
           FieldByName('duration_sec').AsInteger, False), lChNumber, lShowTitle,
           lShowColor);
-
       except
         inc(lShowCount);
         if lShowCount = 10 then
           Break;
-
         MainDataModule.DreamFDMemTableTextEPG.Next;
-
       end;
       inc(lShowCount);
-      if lShowCount = 10 then
+      if lShowCount = vChMax then
         Break;
-
       MainDataModule.DreamFDMemTableTextEPG.Next;
     end;
-
     inc(lChNumber);
-    if lChNumber = 10 then
+    if lChNumber = vChMax then
       Break;
     MainDataModule.DreamFDMemTableChannelList.Next;
   end;
+end;
 
-  { fGanttSeriesGuides.AddGanttColor(0, 20, 0, 'test', TAlphaColorRec.Beige);
-    fGanttSeriesGuides.AddGanttColor(20, 30, 0, 'blim', TAlphaColorRec.Honeydew); }
+procedure TMultiEPGFrame.ChartScroll(Sender: TObject);
+begin
+  { if (fChart.BottomAxis.Minimum < fGanttSeriesGuidesMin) then
+    begin
+    fChart.BottomAxis.Minimum := fGanttSeriesGuidesMin;
+    fChart.BottomAxis.Maximum := fChart.BottomAxis.Minimum +
+    fGanttSeriesGuidesMax;
+    end;
+    if (fChart.BottomAxis.Maximum > 70) then
+    begin
+    fChart.BottomAxis.Minimum := 20;
+    fChart.BottomAxis.Maximum := 70;
+    end; }
+
+  if (fChart.LeftAxis.Minimum < fGanttSeriesChannelsMin) then
+  begin
+    fChart.LeftAxis.Minimum := fGanttSeriesChannelsMin;
+    fChart.LeftAxis.Maximum := fGanttSeriesChannelsMax;
+  end;
+
+  if (fChart.LeftAxis.Maximum > fChannelNumber - 0.5) then
+  begin
+    fChart.LeftAxis.Maximum := fChannelNumber - 0.5;
+    fChart.LeftAxis.Minimum := fChart.LeftAxis.Maximum - fChannelNumber + 0.5;
+    // fGanttSeriesChannels.Clear;
+    // fGanttSeriesGuides.Clear;
+    // loadNextChannels(7, 14);
+  end;
+
+end;
+
+procedure TMultiEPGFrame.init;
+var
+  lChMin: Integer;
+begin
+  fChart := TChart.Create(self);
+  fChart.Parent := self;
+  fChart.Align := TAlignLayout.Client;
+
+  fChart.View3D := False;
+  fChart.BottomAxis.Visible := True;
+  fChart.BottomAxis.OtherSide := True;
+
+  fChart.Frame.Visible := False;
+  fChart.Width := self.Width;
+  fChart.Height := self.Height;
+  fChart.MarginRight := 0;
+  fChart.MarginBottom := 0;
+  fChart.MarginLeft := 0;
+  fChart.MarginTop := 1;
+  fChart.LeftAxis.LabelsSize := 80;
+
+  fChart.Legend.Visible := False;
+  fChart.Border.Visible := False;
+
+  fChart.Color := TAlphaColorRec.White;
+
+  fChart.LeftAxis.Inverted := True;
+  fChart.LeftAxis.TickOnLabelsOnly := True;
+
+  fChart.LeftAxis.MinorTicks.Visible := False;
+  fChart.BottomAxis.MinorTicks.Visible := False;
+
+  fChart.AllowZoom := False;
+  fChart.AllowPanning := TPanningMode.pmBoth;
+  fChart.ScrollMouseButton := TMouseButton.mbLeft;
+  fChart.OnScroll := self.ChartScroll;
+
+  // fChannelNumber := MainDataModule.DreamFDMemTableChannelList.RecordCount;
+  fChannelNumber := 7;
+
+  fChMin := 0;
+  fChMax := fChannelNumber;
+
+  fGanttSeriesChannelsMin := -0.5;
+  fGanttSeriesChannelsMax := fChannelNumber - 1;
+
+  fChart.LeftAxis.Automatic := False;
+  fChart.LeftAxis.Minimum := fGanttSeriesChannelsMin;
+  fChart.LeftAxis.Maximum := fGanttSeriesChannelsMax;
+
+  fGanttSeriesGuidesMin := now;
+  fGanttSeriesGuidesMax := IncMinute(fGanttSeriesGuidesMin, 120);
+
+  fChart.BottomAxis.Automatic := False;
+  fChart.BottomAxis.Maximum := fGanttSeriesGuidesMax;
+  fChart.BottomAxis.Minimum := fGanttSeriesGuidesMin;
+
+  fGanttSeriesChannels := TGanttSeries.Create(self);
+  fGanttSeriesGuides := TGanttSeries.Create(self);
+  fLineSeriesNow := TLineSeries.Create(self);
+
+  if not MainDataModule.DreamFDMemTableChannelList.Active then
+  begin
+    MainDataModule.DreamFDMemTableChannelList.Open;
+    MainDataModule.DreamFDMemTableChannelList.First;
+  end;
+
+  // loading the next channels
+  loadNextChannels(0, fChannelNumber);
 
   fLineSeriesNow.AddXY(now, fGanttSeriesChannelsMin, '',
     TAlphaColorRec.Darksalmon);
